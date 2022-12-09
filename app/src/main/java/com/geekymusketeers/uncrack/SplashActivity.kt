ca5a5669a40c75796976cb2b1a3f0ef303ddb5b8
Aritra Das <aritrarick2002@gmail.com>
2022-12-09 22:17:12 +0530

    Splash Screen Added 🐌

--- app/src/main/java/com/geekymusketeers/uncrack/SplashActivity.kt
+++ app/src/main/java/com/geekymusketeers/uncrack/SplashActivity.kt
@@ -1,11 +1,25 @@
 package com.geekymusketeers.uncrack
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.geekymusketeers.uncrack.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, FingerPrintActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1000)
     }
 }
