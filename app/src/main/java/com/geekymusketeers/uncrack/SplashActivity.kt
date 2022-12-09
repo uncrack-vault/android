9af6526adb8aa10cd60fd309de8af94adf219e20
Aritra Das <aritrarick2002@gmail.com>
2022-12-09 21:37:19 +0530

    Added ViewBinding and RoomDB dependency

--- app/src/main/java/com/geekymusketeers/uncrack/SplashActivity.kt
+++ app/src/main/java/com/geekymusketeers/uncrack/SplashActivity.kt
@@ -3,9 +3,9 @@ package com.geekymusketeers.uncrack
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
     }
 }
