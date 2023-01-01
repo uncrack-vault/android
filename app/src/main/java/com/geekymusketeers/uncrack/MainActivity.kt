7b72daf22f5b998fa338d6eeead372fcc2c7437f
Aritra Das <aritrarick2002@gmail.com>
2023-01-02 01:44:30 +0530

    Added Navigation Code ✅

--- app/src/main/java/com/geekymusketeers/uncrack/MainActivity.kt
+++ app/src/main/java/com/geekymusketeers/uncrack/MainActivity.kt
@@ -2,10 +2,18 @@ package com.geekymusketeers.uncrack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val bottomNav : BottomNavigationView = findViewById(R.id.bottom_nav)
+        bottomNav.setupWithNavController(navController)
     }
 }
