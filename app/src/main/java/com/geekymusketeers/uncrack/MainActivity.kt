5bb025ae32db5791a050606ffbf95b1b099020e8
Aritra Das <aritrarick2002@gmail.com>
2023-01-03 22:05:36 +0530

    Bottom Navigation Bar working properly and make some changes in Generate Password fragment 👀

--- app/src/main/java/com/geekymusketeers/uncrack/MainActivity.kt
+++ app/src/main/java/com/geekymusketeers/uncrack/MainActivity.kt
@@ -3,7 +3,10 @@ package com.geekymusketeers.uncrack
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.geekymusketeers.uncrack.databinding.ActivityMainBinding
 import com.geekymusketeers.uncrack.fragments.GeneratePasswordFragment
@@ -13,23 +16,29 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    lateinit var bottomNav: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        replaceFragment(HomeFragment())
+        loadFragment(HomeFragment())
 
         //Hides action bar
         supportActionBar?.hide()
 
-        binding.bottomNav.setOnItemSelectedListener {
-
-            when(it.groupId){
-                R.id.homeFragment -> replaceFragment(HomeFragment())
-                R.id.passwordFragment -> replaceFragment(GeneratePasswordFragment())
-
-                else ->{
+        bottomNav = binding.bottomNav
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.homeFragment -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.passwordFragment -> {
+                    loadFragment(GeneratePasswordFragment())
+                    true
+                }
+                else -> {
 
                 }
             }
@@ -37,11 +46,9 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun replaceFragment(fragment: Fragment){
-
-        val fragmentManager = supportFragmentManager
-        val fragmentTransition = fragmentManager.beginTransaction()
-        fragmentTransition.replace(R.id.fragment,fragment)
-        fragmentTransition.commit()
+    private fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment, fragment)
+        transaction.commit()
     }
 }
