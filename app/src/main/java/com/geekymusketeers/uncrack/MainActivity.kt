2b8ba301337f4734e516bd08e53ef67e17be66c7
Aritra Das <aritrarick2002@gmail.com>
2023-01-03 17:30:17 +0530

    Fixed the bug happening due to Dependency Injection  ✅

--- app/src/main/java/com/geekymusketeers/uncrack/MainActivity.kt
+++ app/src/main/java/com/geekymusketeers/uncrack/MainActivity.kt
@@ -13,9 +13,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-        val bottomNav : BottomNavigationView = findViewById(R.id.bottom_nav)
-        bottomNav.setupWithNavController(navController)
+//        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
+//        val navController = navHostFragment.navController
+//        val bottomNav : BottomNavigationView = findViewById(R.id.bottom_nav)
+//        bottomNav.setupWithNavController(navController)
     }
 }
