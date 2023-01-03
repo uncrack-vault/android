7f9424cf0a2335c1e5c1ad5a61d135cb13d3045f
Aritra Das <aritrarick2002@gmail.com>
2023-01-03 17:53:36 +0530

    Fixed the bug in Bottom Navigation Bar

--- app/src/main/java/com/geekymusketeers/uncrack/MainActivity.kt
+++ app/src/main/java/com/geekymusketeers/uncrack/MainActivity.kt
@@ -2,23 +2,46 @@ package com.geekymusketeers.uncrack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.geekymusketeers.uncrack.databinding.ActivityMainBinding
+import com.geekymusketeers.uncrack.fragments.GeneratePasswordFragment
+import com.geekymusketeers.uncrack.fragments.HomeFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
 
         //Hides action bar
         supportActionBar?.hide()
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-        val bottomNav : BottomNavigationView = findViewById(R.id.bottom_nav)
-        bottomNav.setupWithNavController(navController)
+        binding.bottomNav.setOnItemSelectedListener {
+
+            when(it.groupId){
+                R.id.homeFragment -> replaceFragment(HomeFragment())
+                R.id.passwordFragment -> replaceFragment(GeneratePasswordFragment())
+
+                else ->{
+
+                }
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransition = fragmentManager.beginTransaction()
+        fragmentTransition.replace(R.id.fragment,fragment)
+        fragmentTransition.commit()
     }
 }
