fd846b84539ec9c7a0694d0d9645e7320f90a2e9
Aritra Das <aritrarick2002@gmail.com>
2023-01-04 17:00:24 +0530

    Added spinner in the Add Fragment ✅

--- app/src/main/java/com/geekymusketeers/uncrack/repository/AccountRepository.kt
+++ app/src/main/java/com/geekymusketeers/uncrack/repository/AccountRepository.kt
@@ -3,9 +3,7 @@ package com.geekymusketeers.uncrack.repository
 import androidx.lifecycle.LiveData
 import com.geekymusketeers.uncrack.data.AccountDao
 import com.geekymusketeers.uncrack.model.Account
-import com.geekymusketeers.uncrack.data.AccountDatabase
-import kotlinx.coroutines.flow.Flow
-import javax.inject.Inject
+
 
 class AccountRepository(private val accountDao: AccountDao) {
     val readAllAccount: LiveData<List<Account>> = accountDao.readAllAccount()
