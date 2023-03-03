07cb04cdb17205ff5485118d735e6d491461dd97
Aritra Das <aritrarick2002@gmail.com>
2023-03-03 21:54:04 +0530

    Created the Adapter class for Cards 💳.

---
+++ app/src/main/java/com/geekymusketeers/uncrack/adapter/CardAdapter.kt
@@ -0,0 +1,51 @@
+package com.geekymusketeers.uncrack.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.geekymusketeers.uncrack.R
+import com.geekymusketeers.uncrack.data.model.Card
+import java.util.EventListener
+
+class CardAdapter(private val listener: (Card) -> Unit):
+    RecyclerView.Adapter<CardAdapter.ViewHolder>()
+{
+    private var cardList = emptyList<Card>()
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.card_layout,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return cardList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        val currentCard = cardList[position]
+        holder.itemView.setOnClickListener {
+            listener(currentCard)
+        }
+        holder.itemView.findViewById<TextView>(R.id.card_number).text = currentCard.cardNumber
+        holder.itemView.findViewById<TextView>(R.id.expiry_date).text = currentCard.expirationDate
+        holder.itemView.findViewById<TextView>(R.id.card_holder_name).text = currentCard.cardHolderName
+
+    }
+
+    fun setCardData(card: List<Card>){
+        cardList = card
+        notifyDataSetChanged()
+    }
+}
