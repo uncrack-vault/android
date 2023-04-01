acf6cd1346738a0c04d6eb414216294966e61e8a
Aritra Das <aritrarick2002@gmail.com>
2023-04-01 20:14:42 +0530

    Made some changes

--- app/src/main/java/com/geekymusketeers/uncrack/ui/MainActivity.kt
+++ app/src/main/java/com/geekymusketeers/uncrack/ui/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.geekymusketeers.uncrack.ui
 
 
-import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
@@ -13,19 +11,16 @@ import com.geekymusketeers.uncrack.R
 import com.geekymusketeers.uncrack.databinding.ActivityMainBinding
 import com.geekymusketeers.uncrack.databinding.UpdatemodalBinding
 import com.geekymusketeers.uncrack.ui.fragments.GeneratePasswordFragment
-import com.geekymusketeers.uncrack.ui.fragments.SettingsFragment
+import com.geekymusketeers.uncrack.ui.fragments.settings.SettingsFragment
 import com.geekymusketeers.uncrack.ui.fragments.account.HomeFragment
 import com.geekymusketeers.uncrack.ui.fragments.card.CardFragment
 import com.geekymusketeers.uncrack.util.Util
 import com.geekymusketeers.uncrack.util.Util.Companion.createBottomSheet
 import com.geekymusketeers.uncrack.util.Util.Companion.setBottomSheet
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.play.core.appupdate.AppUpdateInfo
 import com.google.android.play.core.appupdate.AppUpdateManager
 import com.google.android.play.core.appupdate.AppUpdateManagerFactory
-import com.google.android.play.core.install.model.AppUpdateType
-import com.google.android.play.core.install.model.InstallStatus
 import com.google.android.play.core.install.model.UpdateAvailability
 import com.google.android.play.core.tasks.Task
 
