7a4cceb3d4ec062f8ed86bfe3773825ad15779f1
Aritra Das <aritrarick2002@gmail.com>
2023-04-03 21:21:27 +0530

    Created the Master Key Screen

---
+++ app/src/main/java/com/geekymusketeers/uncrack/ui/auth/fragment/MasterKeyFragment.kt
@@ -0,0 +1,21 @@
+package com.geekymusketeers.uncrack.ui.auth.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.geekymusketeers.uncrack.R
+
+
+class MasterKeyFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_master_key, container, false)
+    }
+
+}
---
+++ app/src/main/java/com/geekymusketeers/uncrack/ui/auth/screen/MasterKeyActivity.kt
@@ -0,0 +1,12 @@
+package com.geekymusketeers.uncrack.ui.auth.screen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.geekymusketeers.uncrack.R
+
+class MasterKeyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_master_key)
+    }
+}
