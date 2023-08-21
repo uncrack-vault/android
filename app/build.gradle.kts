e6752b9851179a9987605f21b9e5fc4fe63ba00e
Aritra Das <aritrarick2002@gmail.com>
2023-08-21 20:46:37 +0530

    Bumped the versionName and code.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.geekymusketeers.uncrack"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.5"
+        versionCode = 6
+        versionName = "1.5.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
