6861d63ee9183cf28a571980d727cda2c07fa75d
Aritra Das <aritrarick2002@gmail.com>
2023-08-29 19:00:37 +0530

    Make the fragment non-nullable

--- app/src/main/java/com/geekymusketeers/uncrack/ui/auth/master_key/LockMasterKeyFragment.kt
+++ app/src/main/java/com/geekymusketeers/uncrack/ui/auth/master_key/LockMasterKeyFragment.kt
@@ -36,7 +36,7 @@ class LockMasterKeyFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         _binding = FragmentLockMasterKeyBinding.inflate(inflater,container,false)
 
