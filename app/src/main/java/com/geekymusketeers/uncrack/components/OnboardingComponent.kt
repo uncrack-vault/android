db54b734b7fe3e7355f678fb928ccde5423297cd
aritra <aritrarick2002@gmail.com>
2024-03-01 14:54:30 +0530

    Fixed the UI of Onboarding component

--- app/src/main/java/com/geekymusketeers/uncrack/components/OnboardingComponent.kt
+++ app/src/main/java/com/geekymusketeers/uncrack/components/OnboardingComponent.kt
@@ -7,20 +7,24 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.geekymusketeers.uncrack.ui.theme.OnPrimaryContainer
+import com.geekymusketeers.uncrack.ui.theme.OnSurface60
 import com.geekymusketeers.uncrack.ui.theme.bold22
 import com.geekymusketeers.uncrack.ui.theme.medium16
 
 @Composable
-fun OnboardingComponent(item: OnBoardingItem) {
+fun OnboardingComponent(
+    modifier: Modifier = Modifier,
+    item: OnBoardingItem
+) {
     Column(
-        modifier = Modifier
-            .fillMaxSize(),
+        modifier = Modifier.then(modifier),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
+
         Image(
             painter = painterResource(id = item.image),
             contentDescription = "onboardimage",
@@ -28,19 +32,22 @@ fun OnboardingComponent(item: OnBoardingItem) {
                 .fillMaxWidth()
                 .height(380.dp)
         )
-        Spacer(modifier = Modifier.padding(50.dp))
+        Spacer(modifier = Modifier.padding(24.dp))
 
         Text(
+            modifier = Modifier.padding(horizontal = 30.dp),
             text = item.title,
-            style = bold22,
-            color = Color.Black,
+            style = bold22.copy(OnPrimaryContainer),
+            textAlign = TextAlign.Center
         )
 
+        Spacer(modifier = Modifier.height(16.dp))
+
         Text(
+            modifier = Modifier.padding(horizontal = 30.dp),
             text = item.text,
             textAlign = TextAlign.Center,
-            style = medium16,
-            color = Color.Black,
+            style = medium16.copy(OnSurface60)
         )
     }
 }
