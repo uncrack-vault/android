2c529fba086878a96d3aa81f2925dad1daada414
aritra <aritrarick2002@gmail.com>
2024-03-02 00:33:08 +0530

    Converted Splash Screen to `ComponentActivity`

--- app/src/main/java/com/geekymusketeers/uncrack/presentation/introScreens/SplashScreen.kt
+++ app/src/main/java/com/geekymusketeers/uncrack/presentation/introScreens/SplashScreen.kt
@@ -1,5 +1,11 @@
 package com.geekymusketeers.uncrack.presentation.introScreens
 
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
@@ -18,6 +24,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -25,10 +33,28 @@ import com.geekymusketeers.uncrack.R
 import com.geekymusketeers.uncrack.ui.theme.Background
 import kotlinx.coroutines.delay
 
+@SuppressLint("CustomSplashScreen")
+class SplashScreen : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                Color.White.toArgb(), Color.White.toArgb()
+            ),
+            navigationBarStyle = SystemBarStyle.light(
+                Color.White.toArgb(), Color.White.toArgb()
+            )
+        )
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            SplashContent()
+        }
+    }
+}
 @Composable
-fun SplashScreen(
-    navController: NavHostController
-) {
+fun SplashContent() {
 
     var animation by remember {
         mutableStateOf(false)
@@ -43,9 +69,7 @@ fun SplashScreen(
 
     LaunchedEffect(true) {
         animation = true
-        delay(1000L)
-        navController.popBackStack()
-        navController.navigate(route = "onboarding_screen")
+        delay(2000L)
     }
 
     Surface {
@@ -59,7 +83,7 @@ fun SplashScreen(
             Image(
                 modifier = Modifier
                     .alpha(alphaAnimation.value)
-                    .size(240.dp),
+                    .size(120.dp),
                 painter = painterResource(id = R.drawable.dark_uncrack_cutout),
                 contentDescription = "uncrack_logo"
             )
