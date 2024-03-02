cd7af6f70f26cca6cae420c83281544f009393b2
aritra <aritrarick2002@gmail.com>
2024-03-02 20:26:37 +0530

    Created the UI of Login Screen

--- app/src/main/java/com/geekymusketeers/uncrack/components/UCTextField.kt
+++ app/src/main/java/com/geekymusketeers/uncrack/components/UCTextField.kt
@@ -29,7 +29,7 @@ import com.geekymusketeers.uncrack.ui.theme.SurfaceVariantLight
 import com.geekymusketeers.uncrack.ui.theme.normal16
 
 @Composable
-fun IONTextField(
+fun UCTextField(
     modifier: Modifier = Modifier,
     headerText: String = "",
     hintText: String = "",
