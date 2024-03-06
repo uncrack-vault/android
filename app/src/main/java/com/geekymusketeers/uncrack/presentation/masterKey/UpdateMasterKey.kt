164fd0a8f2cd7ab09eea71f2040d4e5531b02594
aritra <aritrarick2002@gmail.com>
2024-03-06 21:34:13 +0530

    Created the UI of Update Master Key

--- app/src/main/java/com/geekymusketeers/uncrack/presentation/masterKey/UpdateMasterKey.kt
+++ app/src/main/java/com/geekymusketeers/uncrack/presentation/masterKey/UpdateMasterKey.kt
@@ -1,8 +1,170 @@
 package com.geekymusketeers.uncrack.presentation.masterKey
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.geekymusketeers.uncrack.R
+import com.geekymusketeers.uncrack.components.UCButton
+import com.geekymusketeers.uncrack.components.UCTextField
+import com.geekymusketeers.uncrack.ui.theme.bold30
 
 @Composable
-fun UpdateMasterKey() {
+fun UpdateMasterKey(
+    modifier: Modifier = Modifier
+) {
 
+    Scaffold(
+        modifier.fillMaxSize()
+    ) { paddingValues ->
+
+        var oldMasterKey by remember {
+            mutableStateOf("")
+        }
+
+        var newMasterKey by remember {
+            mutableStateOf("")
+        }
+
+        var confirmMasterKey by remember {
+            mutableStateOf("")
+        }
+
+        var passwordVisibility by remember {
+            mutableStateOf(false)
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(16.dp)
+        ) {
+            Text(
+                text = "Update Master Key",
+                style = bold30.copy(color = Color.Black)
+            )
+
+            Spacer(modifier = Modifier.height(60.dp))
+
+            UCTextField(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                headerText = "Old Master Key",
+                hintText = "****************",
+                value = oldMasterKey,
+                onValueChange = { oldMasterKey = it },
+                visualTransformation = if (passwordVisibility) VisualTransformation.None else PasswordVisualTransformation(),
+                trailingIcon = {
+
+                    val image = if (passwordVisibility)
+                        painterResource(id = R.drawable.visibility_on)
+                    else painterResource(id = R.drawable.visibility_off)
+
+                    IconButton(onClick =
+                    { passwordVisibility = passwordVisibility.not() }
+                    ) {
+                        Icon(
+                            modifier = Modifier.size(24.dp),
+                            painter = image,
+                            contentDescription = null
+                        )
+                    }
+                }
+            )
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            UCTextField(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                headerText = "New Master Key",
+                hintText = "****************",
+                value = newMasterKey,
+                onValueChange = { newMasterKey = it },
+                visualTransformation = if (passwordVisibility) VisualTransformation.None else PasswordVisualTransformation(),
+                trailingIcon = {
+
+                    val image = if (passwordVisibility)
+                        painterResource(id = R.drawable.visibility_on)
+                    else painterResource(id = R.drawable.visibility_off)
+
+                    IconButton(onClick =
+                    { passwordVisibility = passwordVisibility.not() }
+                    ) {
+                        Icon(
+                            modifier = Modifier.size(24.dp),
+                            painter = image,
+                            contentDescription = null
+                        )
+                    }
+                }
+            )
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            UCTextField(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                headerText = "Confirm Master Key",
+                hintText = "****************",
+                value = confirmMasterKey,
+                onValueChange = { confirmMasterKey = it },
+                visualTransformation = if (passwordVisibility) VisualTransformation.None else PasswordVisualTransformation(),
+                trailingIcon = {
+
+                    val image = if (passwordVisibility)
+                        painterResource(id = R.drawable.visibility_on)
+                    else painterResource(id = R.drawable.visibility_off)
+
+                    IconButton(onClick =
+                    { passwordVisibility = passwordVisibility.not() }
+                    ) {
+                        Icon(
+                            modifier = Modifier.size(24.dp),
+                            painter = image,
+                            contentDescription = null
+                        )
+                    }
+                }
+            )
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            UCButton(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                text = "Update",
+                onClick = {
+                    // TODO: Perform req operation
+                },
+                enabled = false
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun UpdateKeyPreview() {
+    UpdateMasterKey()
 }
