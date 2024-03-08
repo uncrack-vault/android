03d43e40768108aadd0d6e3404b67ff3a8c3ac41
aritra <aritrarick2002@gmail.com>
2024-03-08 12:45:47 +0530

    Created custom top-app bar

---
+++ app/src/main/java/com/geekymusketeers/uncrack/components/UCTopAppBar.kt
@@ -0,0 +1,59 @@
+package com.geekymusketeers.uncrack.components
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
+import com.geekymusketeers.uncrack.ui.theme.BackgroundLight
+import com.geekymusketeers.uncrack.ui.theme.DMSansFontFamily
+import com.geekymusketeers.uncrack.ui.theme.OnSurfaceLight
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UCTopAppBar(
+    modifier: Modifier = Modifier,
+    title: String = "",
+    navigationIcon: ImageVector = Icons.AutoMirrored.Filled.ArrowBack,
+    shouldShowBackButton: Boolean = true,
+    fontSize: TextUnit = 22.sp,
+    colors: TopAppBarColors = TopAppBarDefaults.topAppBarColors(BackgroundLight),
+    onBackPress: () -> Unit = {},
+) {
+    TopAppBar(
+        modifier = modifier,
+        title = {
+            Text(
+                text = title,
+                fontFamily = DMSansFontFamily,
+                fontSize = fontSize,
+                color = OnSurfaceLight,
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 1,
+            )
+        },
+        navigationIcon = {
+            if (shouldShowBackButton) {
+                IconButton(onClick = {
+                    onBackPress()
+                }) {
+                    Icon(
+                        imageVector = navigationIcon,
+                        contentDescription = null
+                    )
+                }
+            }
+        },
+        colors = colors
+    )
+}
