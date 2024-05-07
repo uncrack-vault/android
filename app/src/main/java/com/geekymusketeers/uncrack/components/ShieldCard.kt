c92a9d9c99daacb06bf4027dc289612ea8ae98bf
aritra <aritrarick2002@gmail.com>
2024-05-07 13:29:44 +0530

    Created the UI of Shield Screen

---
+++ app/src/main/java/com/geekymusketeers/uncrack/components/ShieldCard.kt
@@ -0,0 +1,57 @@
+package com.geekymusketeers.uncrack.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.geekymusketeers.uncrack.ui.theme.medium14
+import com.geekymusketeers.uncrack.ui.theme.medium26
+
+@Composable
+fun ShieldCard(modifier: Modifier = Modifier) {
+
+    Row(
+        modifier = modifier
+            .widthIn(min = 165.dp, 165.dp)
+            .heightIn(min = 99.dp, 99.dp)
+            .clip(RoundedCornerShape(10.dp))
+            .background(Color.White)
+            .shadow(
+                elevation = 5.dp,
+                spotColor = Color(0x0D666666),
+                ambientColor = Color(0x0D666666)
+            )
+            .padding(horizontal = 16.dp, vertical = 17.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically),
+            horizontalAlignment = Alignment.Start,
+        ) {
+            Text(
+                text = "12",
+                color = Color.Green,
+                style = medium26
+
+            )
+            Text(
+                text = "Safe Password",
+                color = Color.Black,
+                style = medium14
+            )
+        }
+    }
+}
--- app/src/main/java/com/geekymusketeers/uncrack/presentation/shield/ShieldScreen.kt
+++ app/src/main/java/com/geekymusketeers/uncrack/presentation/shield/ShieldScreen.kt
@@ -4,10 +4,16 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -17,23 +23,25 @@ import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.unit.dp
+import com.geekymusketeers.uncrack.components.ShieldCard
 import com.geekymusketeers.uncrack.ui.theme.OnPrimaryContainerLight
 import com.geekymusketeers.uncrack.ui.theme.SurfaceTintLight
 import com.geekymusketeers.uncrack.ui.theme.SurfaceVariantLight
+import com.geekymusketeers.uncrack.ui.theme.bold36
 import com.geekymusketeers.uncrack.ui.theme.normal12
+import com.geekymusketeers.uncrack.ui.theme.normal14
 
 @Composable
 fun ShieldScreen(
     modifier: Modifier = Modifier
 ) {
     val progressValue by remember { mutableFloatStateOf(0f) }
-    val progressMessage by rememberSaveable { mutableStateOf("") }
+    val progressMessage by rememberSaveable { mutableStateOf("0") }
 
     Scaffold(
         modifier = modifier.fillMaxWidth()
@@ -44,10 +52,14 @@ fun ShieldScreen(
                 .fillMaxSize()
                 .padding(paddingValues)
                 .background(SurfaceVariantLight)
-                .padding(16.dp)
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
 
-            Box(contentAlignment = Alignment.Center) {
+            Box(
+                modifier = Modifier.padding(top = 30.dp),
+                contentAlignment = Alignment.Center
+            ) {
 
                 CircularProgressIndicator(
                     progress = { progressValue },
@@ -59,13 +71,31 @@ fun ShieldScreen(
                 )
 
                 Text(
+                    modifier = Modifier.padding(bottom = 20.dp),
                     text = progressMessage,
-                    style = normal12,
-                    color = OnPrimaryContainerLight,
+                    style = bold36,
+                    color = Color.Black,
                 )
 
+                Text(
+                    modifier = Modifier.padding(top = 90.dp),
+                    text = "OUT OF 100",
+                    style = normal14.copy(color = SurfaceTintLight)
+                )
             }
 
+            Spacer(modifier = Modifier.height(30.dp))
+
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                verticalArrangement = Arrangement.spacedBy(22.dp),
+                horizontalArrangement = Arrangement.spacedBy(14.dp),
+                content = {
+                    items(4) {
+                        ShieldCard()
+                    }
+                }
+            )
         }
     }
 }
