44be8fee510c69409102030ba32d5817905fab2c
aritra <aritrarick2002@gmail.com>
2024-05-12 13:05:07 +0530

    Impl. logic to block screen short

--- app/src/main/java/com/geekymusketeers/uncrack/UnCrackApplication.kt
+++ app/src/main/java/com/geekymusketeers/uncrack/UnCrackApplication.kt
@@ -1,6 +1,9 @@
 package com.geekymusketeers.uncrack
 
+import android.app.Activity
 import android.app.Application
+import android.os.Bundle
+import android.view.WindowManager
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
@@ -9,8 +12,27 @@ class UnCrackApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        registerActivityLifecycle()
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
+    }
+
+    private fun registerActivityLifecycle() {
+        registerActivityLifecycleCallbacks(object : ActivityLifecycleCallbacks {
+            override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+                activity.window.setFlags(
+                    WindowManager.LayoutParams.FLAG_SECURE,
+                    WindowManager.LayoutParams.FLAG_SECURE
+                )
+            }
+
+            override fun onActivityStarted(activity: Activity) {}
+            override fun onActivityResumed(activity: Activity) {}
+            override fun onActivityPaused(activity: Activity) {}
+            override fun onActivityStopped(activity: Activity) {}
+            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {}
+            override fun onActivityDestroyed(activity: Activity) {}
+        })
     }
 }
