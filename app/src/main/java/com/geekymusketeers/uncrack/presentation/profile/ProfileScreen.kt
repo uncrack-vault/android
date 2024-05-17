a06e8cb24ea323aefa1879e16b1ff4a4744c82f6
aritra <aritrarick2002@gmail.com>
2024-05-17 21:06:21 +0530

    Impl the CVS Reader func

--- app/src/main/java/com/geekymusketeers/uncrack/presentation/profile/ProfileScreen.kt
+++ app/src/main/java/com/geekymusketeers/uncrack/presentation/profile/ProfileScreen.kt
@@ -16,7 +16,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -94,14 +93,6 @@ fun ProfileScreen(
             HorizontalDivider(
                 thickness = 2.dp,
                 color = SurfaceVariantLight
-            )
-
-            UCSettingsCard(
-                itemName = "Import",
-                iconId = R.drawable.import_icon,
-                onClick = {
-
-                }
             )
 
             HorizontalDivider(
