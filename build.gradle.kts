e37db8bf1e4d9ec7a641b14fd82fc17ae4d13759
aritra-tech <aritrarick2002@gmail.com>
2025-03-06 21:46:44 +0530

    feat: updated apg and gradle plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 buildscript {
     dependencies {
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")
-        classpath("com.android.tools.build:gradle:7.4.2")
+        classpath("com.android.tools.build:gradle:8.9.0")
         classpath("com.google.gms:google-services:4.4.1")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.9")
     }
 }
 plugins {
-    id("com.android.application") version "8.2.2" apply false
+    id("com.android.application") version "8.9.0" apply false
     id("com.android.library") version "8.0.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
     id("com.google.dagger.hilt.android") version "2.49" apply false
