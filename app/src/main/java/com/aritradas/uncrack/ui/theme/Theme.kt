352b92a74d56b36321b8f527508c16bd3692d6f7
aritra-tech <aritrarick2002@gmail.com>
2025-05-10 10:04:33 +0530

    fix: Status bar color

--- app/src/main/java/com/aritradas/uncrack/ui/theme/Theme.kt
+++ app/src/main/java/com/aritradas/uncrack/ui/theme/Theme.kt
@@ -9,16 +9,14 @@ import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
-import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
-import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.aritradas.uncrack.sharedViewModel.ThemeViewModel
 
@@ -68,6 +66,7 @@ fun UnCrackTheme(
         SideEffect {
             val window = (view.context as Activity).window
             window.statusBarColor =  colorScheme.background.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = true
         }
     }
 
