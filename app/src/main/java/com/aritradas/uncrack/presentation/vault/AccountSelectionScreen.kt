3bf70cba6afe1ca49aeba2e16f08412f5bb2f0ab
aritra-tech <aritrarick2002@gmail.com>
2025-05-10 10:07:43 +0530

    feat: styling updates

--- app/src/main/java/com/aritradas/uncrack/presentation/vault/AccountSelectionScreen.kt
+++ app/src/main/java/com/aritradas/uncrack/presentation/vault/AccountSelectionScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -44,7 +45,8 @@ fun AccountSelectionScreen(
             UCTopAppBar(
                 modifier = Modifier.fillMaxWidth(),
                 title = "Add Account",
-                onBackPress = { navController.popBackStack() }
+                onBackPress = { navController.popBackStack() },
+                colors = TopAppBarDefaults.topAppBarColors(BackgroundLight)
             )
         }
     ) { paddingValues ->
